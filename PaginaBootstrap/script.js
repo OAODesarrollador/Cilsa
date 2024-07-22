b93637e4db50559b16ad67df145f0f4ee9b37e9a
oaodesarrollador <oaodesarrollador@gmail.com>
2024-07-21 22:10:22 -0300

    Subiendo Pagina con Bootstrap

--- PaginaBootstrap/script.js
+++ PaginaBootstrap/script.js
@@ -1,43 +1,52 @@
-const formulario = document.getElementById('#miFormulario');
-const validar = document.getElementById('#enviar');
+const formulario = document.getElementById('miFormulario');
+const validar = document.getElementById('enviar');
+const barraMenu = document.getElementById('menu');
 
-// *---------- Cambio de Modo Oscuro-Claro
+
+// ---------------------- Cambio Modo --------------------
 
 const claro = () => {
     document.querySelector('body').setAttribute('data-bs-theme','dark')
     document.querySelector('#iconoModo').setAttribute('class', 'bi bi-sun-fill')
+    document.querySelector('#next').style.color = 'white';
+    document.querySelector('#prev').style.color = 'white';
+    document.querySelector('#menu').style.backgroundColor = '#4e4e4e';
+   
 }
 const oscuro = () => {
     document.querySelector('body').setAttribute('data-bs-theme','light')
     document.querySelector('#iconoModo').setAttribute('class', 'bi bi-moon-fill')
+    document.querySelector('#next').style.color = 'black';
+    document.querySelector('#prev').style.color = 'black';
+    document.querySelector('#menu').style.backgroundColor = '#dbd9d9';
 }
 const cambiarTema  = () => {
     document.querySelector('body').getAttribute('data-bs-theme') === 'dark'?
     oscuro() : claro();
 }
 
-
 //  - - - - -  Cartel Alerta de los campos
 
-    function alert(campo, mensaje, tipo) {
-        var wrapper = document.createElement('div')
-        wrapper.innerHTML = '<div class="alert alert-' + tipo + ' alert-dismissible" role="alert">' +'<i class="bi bi-exclamation-triangle"></i>' +"  "+ mensaje + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
-        var campo = document.getElementById(campo);
-        campo.append(wrapper);
-        
-        }
+function alert(campo, mensaje, tipo) {
+    var wrapper = document.createElement('div')
+    wrapper.innerHTML = '<div class="alert alert-' + tipo + ' alert-dismissible" role="alert">' +'<i class="bi bi-exclamation-triangle"></i>' +"  "+ mensaje + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+    var campo = document.getElementById(campo);
+    campo.append(wrapper);
+    
+    }
 
 // --------- Validacion de los datos ---------------
 
 
 validar.onclick = (e) => {
     e.preventDefault();
-    
+    console.log("si");
     const vnombre = document.getElementById('nombre').value;
     const vapellido = document.getElementById('apellido').value;
     const vemail = document.getElementById('email').value;
-    const vnacimiento = document.getElementById('nacimiento').value;
-    const vresidencia = document.getElementById('residencia').value;
+    var modal = document.getElementById('contacto');
+    var modalInstancia = bootstrap.Modal.getInstance(modal);
+    
     let isValid = true;
 
     if( vnombre == null || vnombre.length == 0 || /^\s+$/.test(vnombre) ) { 
@@ -47,33 +56,35 @@ validar.onclick = (e) => {
     }
     if( vapellido == null || vapellido.length == 0 || /^\s+$/.test(vapellido) ) { 
         alert('errorApellido', "Debe ingresar datos en el Apellido", 'danger');
+        isValid = false;
     return 
     }
     if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(vemail)) ) { 
         alert('errorEmail', "Debe ingresar formato válido en el E-mail. valor@valor.valor.valor ", 'danger');
+        isValid = false;
     return 
     }
-    if( !isNaN(vnacimiento) ) {
-        alert("errorFecha", "Debe ingresar datos en la Fecha", 'danger'); 
-    return
-    }
-    if( vresidencia == null || vresidencia.length == 0 || /^\s+$/.test(vresidencia) ) { 
-        alert('errorResidencia', 'Debes ingresar datos de la Residencia', 'danger')
-    return 
-    }
+    
     if (isValid) {
+        console.log("si");
         confirmacion();
-        // Enviar el formulario después de mostrar la confirmación
+        // Ajusta el tiempo según sea necesario
         setTimeout(() => {
-            formulario.submit();
-        }, 3000); // Ajusta el tiempo según sea necesario
-    } 
+            modalInstancia.hide(); // Oculta el formulario
+        }, 2300); 
+        
+        formulario.submit(); 
+        formulario.reset();// Borra los datos del formulario
+       
+    }
+   
+   
 }
     
 function confirmacion() {
     document.getElementById('alertaExito').style.display = 'block';   
     setTimeout(function() {
         document.getElementById('alertaExito').style.display = 'none';
-    }, 3000);
+    }, 2000);
     
     }
